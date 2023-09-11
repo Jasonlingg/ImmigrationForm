afaac3b5e02f1cc39abc8031ffec70305747abbc
Jason Ling <105073210+Jasonlingg@users.noreply.github.com>
2023-09-11 16:03:53 -0400

    no tests for now

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+require('react-datepicker/dist/react-datepicker.css');
 
 test('renders learn react link', () => {
   render(<App />);
